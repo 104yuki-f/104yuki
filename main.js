967a1527b312bc2ea6030472a04b76583e9f95f1
104yuki-f <98097027+104yuki-f@users.noreply.github.com>
2022-01-27 08:32:53 +0900

    Update main.js

--- main.js
+++ main.js
@@ -43,7 +43,7 @@ window.onload=()=>{
 	function setItem(index){
 		entry.value='';
 		ja.textContent=animals[index][0];
-		img.src='images/'+animals[index][2];
+		img.src='https://github.com/104yuki-f/104yuki-f/blob/main/images/'+animals[index][2];
 	}
 	setItem(index);
 }
