6384e31b6d09785c8ca1b57d12d218234bcfecec
104yuki-f <98097027+104yuki-f@users.noreply.github.com>
2022-02-09 21:05:50 +0900

    Update vegas.js

--- js/vegas.js
+++ js/vegas.js
@@ -2,9 +2,9 @@ $(function() {
 
     $('#mainimg').vegas({
         slides: [
-            { src: './images/coffee.jpg' },	//1枚目の写真指定
-            { src: './images/cocoa.jpg' },	//2枚目の写真指定
-            { src: './images/cacao.jpg' },	//3枚目の写真指定
+            { src: 'https://github.com/104yuki-f/104yuki-f/blob/main/coffee.jpg' },	//1枚目の写真指定
+            { src: 'https://github.com/104yuki-f/104yuki-f/blob/main/images/cocoa.jpg' },	//2枚目の写真指定
+            { src: 'https://github.com/104yuki-f/104yuki-f/blob/main/images/cacao.jpg' },	//3枚目の写真指定
         ],
 			//写真上にドットを重ねる。不要ならこの１行を削除して下さい。
 		transition: 'fade',			//https://vegas.jaysalvat.com/documentation/transitions/から好みのtransitionを選んで置き換えられます。
